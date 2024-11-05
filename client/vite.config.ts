2fd80a4e98391209897d15cfa88000a976bd922e
npham49 <nguyenphamswork@gmail.com>
2024-11-04 18:29:23 -0800

    adding favicon as a custom route

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(() => {
   return {
-    base: env.VITE_FRONTEND_BASEPATH ? env.VITE_FRONTEND_BASEPATH : "/app",
     plugins: [vue()],
     resolve: {
       alias: {
