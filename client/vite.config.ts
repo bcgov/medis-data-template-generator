05ce6bf4e153ba871fd229437958c0ff6348c543
npham49 <nguyenphamswork@gmail.com>
2024-11-04 18:08:28 -0800

    moving to app folder for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_FRONTEND_BASEPATH ? env.VITE_FRONTEND_BASEPATH : "/app",
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
