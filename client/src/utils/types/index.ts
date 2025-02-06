d20c7f1cb28a93ed0a7f32b7c9dc541acea1d30f
npham49 <nguyenphamswork@gmail.com>
2025-02-06 15:57:52 -0800

    adding new name abbreviation

--- client/src/utils/types/index.ts
+++ client/src/utils/types/index.ts
@@ -35,7 +35,7 @@ export const haMapping = {
   "Vancouver Island": "VIHA",
   Northern: "NHA",
   "Vancouver Coastal": "VCHA",
-  "Nisga'a": "Nisga'a",
+  "Nisga'a": "NVHA",
   NotAvailable: "N-A",
 };
 
